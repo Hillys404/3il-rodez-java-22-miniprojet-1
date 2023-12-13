7a1c528fcd67d08c8cbff6f73c6bf08876b35478
Jade CAUSSE <j.causse@formation-cci12.fr>
2023-12-13 17:00:13 +0100

    Reponse à la première question et ajout des set/get de la resolution dans la classe Bruit2D.java

--- src/fr/ecole3il/rodez2023/perlin/math/Bruit2D.java
+++ src/fr/ecole3il/rodez2023/perlin/math/Bruit2D.java
@@ -5,18 +5,22 @@ package fr.ecole3il.rodez2023.perlin.math;
  * Elle définit des méthodes permettant d'obtenir des valeurs de bruit en deux dimensions.
  */
 public abstract class Bruit2D {
-    
+
     /** La graine utilisée pour la génération du bruit */
     private final long graine;
-    
+
+    /** Resolution du bruit à générer */
+    private double resolution;
+
     /**
-     * Constructeur de la classe Noise2D.
-     * @param seed2 La graine utilisée pour initialiser le générateur de bruit.
+     * Constructeur de la classe Bruit2D.
+     * @param graine La graine utilisée pour initialiser le générateur de bruit.
      */
     public Bruit2D(long graine, double resolution) {
         this.graine = graine;
+        this.resolution = resolution;
     }
-    
+
     /**
      * Méthode abstraite pour obtenir la valeur de bruit en 2D pour les coordonnées spécifiées.
      * @param x Coordonnée x pour laquelle obtenir le bruit.
@@ -33,4 +37,15 @@ public abstract class Bruit2D {
         return graine;
     }
 
+    /**
+     * Retourne la resolution du bruit à générer
+     * @return la valeur de la resolution
+     */
+    public double getResolution() { return this.resolution;}
+
+    /**
+     * Modifie la valeur de la resolution du bruit
+     * @param resolution : nouvelle resolution du bruit
+     */
+    public void setResolution(double resolution) { this.resolution = resolution;}
 }
